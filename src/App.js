341df479391701de61cb0de436fd1b910b3f289c
Tamil Selvi <tamilselvi072001@gmail.com>
2024-05-29 09:14:07 +0530

    Structure for header and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Footer from './AppFooter/Footer';
 import AppRoute from './AppRoute';
 
 
 function App() {
   return (
+    <div>
     <div className='full-first-page'><AppRoute /></div>
+    <Footer />
+    </div>
   );
 }
 
--- src/ContactPage/Contact.js
+++ src/ContactPage/Contact.js
@@ -1,8 +1,35 @@
 import React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Item from "antd/es/list/Item";
+import { Button, TextField } from "@mui/material";
+import "./Contact.css"
 
 const Contact = () => {
-  return <div>
-   <div className="common-heading"> Got any Question?</div>
-  </div>;
+  return (
+    <div className="contact-page">
+      <div className="common-heading"> Got Any Question?</div>
+      <Box sx={{ flexGrow: 4}} className="contact-container" >
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+          <TextField fullWidth  label="Name" variant="outlined" />
+          </Grid>
+          <Grid item xs={6}>
+          <TextField  label="Email" fullWidth  variant="outlined" />
+          </Grid>
+          <Grid item xs={12}>
+          <TextField 
+          label="Message"
+          multiline
+          rows={4}
+          maxRows={4} fullWidth variant="outlined" />
+          </Grid>
+        </Grid>
+      </Box>
+      <button className="common-btn contact-send-btn">SEND</button>
+    </div>
+  );
 };
 export default Contact;
